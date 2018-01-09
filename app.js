50ce6d473e972ab85f3a1c8535060e5b7846e7b5
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-01-08 22:41:47 -0500

    add an api

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const keyPublishable = process.env.PUBLISHABLE_KEY;
 const keySecret = process.env.SECRET_KEY;
 const port = process.env.PORT;
@@ -6,6 +7,9 @@ const express = require("express");
 const app = express();
 const stripe = require("stripe")(keySecret);
 const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
@@ -66,11 +70,51 @@ app.post("/subscribe", (req, res) => {
   .then(subscription => res.render("subscribe.hbs", {subscription}));
 });
 
+// a json api with stats about membership
+//maybe make this a POST instead of a GET later
+app.get("/statsapi", (req, res) => {
+
+  stripe.subscriptions.list(
+    { limit: 99 },
+    function(err, subscriptions) {
+    // asynchronously called
+
+    var stats = {
+      monthly06 : 0,
+      monthly14: 0,
+      monthly25: 0,
+      monthly50: 0,
+      totalMonthlyAmt: 0,
+      totalMembers: 0
+    }
+    for (var i = 0; i < subscriptions.data.length; i++) {
+
+      if (subscriptions.data[i].plan.id == 'membership-06') {
+        stats.monthly06++;
+      }
+      if (subscriptions.data[i].plan.id == 'membership-14') {
+        stats.monthly14++;
+      }
+      if (subscriptions.data[i].plan.id == 'membership-25') {
+        stats.monthly25++;
+      }
+      if (subscriptions.data[i].plan.id == 'membership-50') {
+        stats.monthly50++;
+      }
+    }
+    stats.totalMonthlyAmt = (stats.monthly06 * 6) + (stats.monthly14 * 14) + (stats.monthly25 * 25) + (stats.monthly50 * 50);
+    stats.totalMembers = stats.monthly06 + stats.monthly14 + stats.monthly25 + stats.monthly50;
+
+    res.send(stats);
+    console.log('response=',stats);
+  });
+
+});//statsapi
+
 // a page with stats about membership
 app.get("/stats", (req, res) => {
 
   stripe.subscriptions.list(
-    // { plan: 'membership-14' },
     { limit: 99 },
     function(err, subscriptions) {
     // asynchronously called
@@ -103,8 +147,7 @@ app.get("/stats", (req, res) => {
       res.render("stats.hbs", stats);
   });
 
-}
-)
+});//stats
 
 
 
